7fc0a8c10eb3acd8c8b81279dfdd45ee78791610
p1ratrulezzz <git@p1ratrulezzz.me>
2018-05-08 17:54:29 +0300

    Update names_helper.js

--- data/names_helper.js
+++ data/names_helper.js
@@ -1,20 +1,29 @@
 (function (jsonFile) {
 
-function window.loadJSONNames(callback) {
+    function loadJSON(callback) {
 
-    var xobj = new XMLHttpRequest();
-    xobj.overrideMimeType("application/json");
-    xobj.open('GET', jsonFile, true);
-    xobj.onreadystatechange = function() {
-        if (xobj.readyState == 4 && xobj.status == "200") {
+        var xobj = new XMLHttpRequest();
+        xobj.overrideMimeType("application/json");
+        xobj.open('GET', jsonFile, true);
+        xobj.onreadystatechange = function() {
+            if (xobj.readyState == 4 && xobj.status == "200") {
 
-            // .open will NOT return a value but simply returns undefined in async mode so use a callback
-            callback(xobj.responseText);
+                // .open will NOT return a value but simply returns undefined in async mode so use a callback
+                callback(xobj.responseText);
 
+            }
         }
-    }
-    xobj.send(null);
+        xobj.send(null);
 
-}
+    }
     
+    window.addEventListener("load", function(event) {
+       loadJSON(function(response) {
+           let data = JSON.parse(response);
+
+           if (window.onnJsonNamesloaded != null) {
+               window.onnJsonNamesloaded(data);
+           }
+       });
+    });
 })('https://raw.githubusercontent.com/p1ratrulezzz/cumberbatch/master/data/names.json');
