e7ae69104e0291e3122cece73830db220ea903f8
p1ratrulezzz <git@p1ratrulezzz.me>
2018-05-18 15:57:59 +0300

    Update names_helper.js

--- js/names_helper.js
+++ js/names_helper.js
@@ -5,19 +5,22 @@
         var xobj = new XMLHttpRequest();
         xobj.overrideMimeType("application/json");
         xobj.open('GET', jsonFile, true);
+        xobj.onerror(function(e) {
+            alert("Error " + e.target.status + " occurred while receiving the document. Try to refresh the page");
+        });
+        
         xobj.onreadystatechange = function() {
             if (xobj.readyState == 4 && xobj.status == "200") {
 
                 // .open will NOT return a value but simply returns undefined in async mode so use a callback
                 callback(xobj.responseText);
-
             }
         }
         xobj.send(null);
 
     }
     
-    window.addEventListener("load", function(event) {
+    document.addEventListener("DOMContentLoaded", function(event) {
        loadJSON(function(response) {
            let data = JSON.parse(response);
 
