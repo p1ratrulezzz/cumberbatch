76b4e98100d35b6c043f258805fcd1cf4feb82ff
p1ratrulezzz <git@p1ratrulezzz.me>
2018-06-03 12:20:55 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -142,10 +142,11 @@
   
   window.initialZoom = function () {
     // Initial zoom 0.28 for resolution of 304500
-    let newZoom = Math.floor(((window.screen.availHeight * window.screen.availWidth * 0.28) / 304500) * 10) / 10;
+    let newZoom = Math.floor(((window.screen.availWidth * 0.28) / 375) * 1000) / 1000;
     document.body.style.zoom = newZoom;
   }
   
-  // document.addEventListener("DOMContentLoaded", initialZoom);
+  document.addEventListener("DOMContentLoaded", initialZoom);
+  document.addEventListener("resize", initialZoom);
   
 })();
